5444b3ac115cc1442a7e887ba9f815e1a3e8c471
karthiivcena <karthii.vreddy@gmail.com>
2023-06-21 22:35:25 +0530

    Program to implement linked list on array

--- 2023_06_21/array_linkedlist.c
+++ 2023_06_21/array_linkedlist.c
@@ -2,18 +2,70 @@
 #include <stdio.h>
 
 Rectangle *rlist_head;                                    /* Pointer Variable of return-type Rectangle(struct) */
-Rectangle rpool_rect[POOLSIZE];                           /* array of return-type Rectangle(struct) */
+Rectangle rpool_nodes[POOLSIZE];                           /* array of return-type Rectangle(struct) */
+
+//Rectangle *rpool_rect;
 
 void clear_node(Rectangle *);
 void *init_rpool(Rectangle **);
 void display_rpool(Rectangle *);
 
+Rectangle *free_pool_node(Rectangle **rpool_ptr,Rectangle *free_node );
+Rectangle *alloc_pool_node(Rectangle **rpool_rect);
+
 /* Main Function */
 int main()
 {
-    int *rlist_head = &rpool_rect;
-    init_rpool(test_arr_ptr_p);
-    display_rpool(rpool_ptr, POOLSIZE);
+    //printf("\n FILE: %s \n LINE : %s \n TIME : %s", __FILE__, __LINE__, __TIME__);
+    int i;
+    rlist_head = &rpool_nodes[0];
+    Rectangle *node[10];
+    init_rpool(&rlist_head);
+    display_rpool(rlist_head);                  /* Calling the Display Function */
+    
+    rlist_head -> id = 1;
+    for(i = 0;i < 10; i++)
+    {
+        node[i] = alloc_pool_node(&rlist_head);
+        printf("\n %d.Pool Index : %x -- %x \n",i,node[i], &(rpool_nodes[node[i] -> pool_index]));
+        display_rpool(rlist_head);                  /* Calling the Display Function */
+    
+    }
+    for(i=0; i<10; i++)
+    {
+    	if(node[i] != &(rpool_nodes[node[i] -> pool_index]))
+    	{
+            printf(" \n Address Mismatch \n");
+    	}
+    }
+                     
+                     /* Calling the Display Function */
+    for(i=9; i>=0; i--)
+    {
+    	free_pool_node(&rlist_head, node[i]);
+    }       /* Calling the Free Pool Function */
+    display_rpool(rlist_head);
+
+}
+
+
+/* Function to free the node allocated to someone of the structure */
+Rectangle *free_pool_node(Rectangle **rpool_ptr,Rectangle *free_node )
+{
+    
+    free_node -> next = *rpool_ptr;
+    *rpool_ptr = free_node;
+}
+
+/* Function to allocate the node to someone of type structure */
+Rectangle *alloc_pool_node(Rectangle **rpool_pptr)
+{
+    
+    Rectangle *node_ptr;
+    node_ptr = *rpool_pptr;
+    *rpool_pptr = node_ptr -> next;
+    node_ptr -> next = NULL;
+    return node_ptr;
 }
 
 /* Function to Clear all the elements of Array - Linked-list */
@@ -23,36 +75,37 @@ void clear_node(Rectangle *r_ptr)
     r_ptr -> id = 0;
     for(i = 0; i < VTX_COUNT; i++)
     {	
-	r_ptr --> vertex[i].x = 0;
-	r_ptr --> vertex[i].y = 0;
+	r_ptr -> vertex[i].x = 0;
+	r_ptr -> vertex[i].y = 0;
     }
 }
 
 /* Function to implement Linked-list for an Array */
-void *init_rpool(Rectangle **rpool_rect)
+void *init_rpool(Rectangle **rpool_pptr)
 {
     int index;
-    
+    //rpool_nodes = (Rectangle *) malloc(sizeof(Rectangle)*(POOLSIZE));
     for(index = 0;index < POOLSIZE - 1; index++)
     {
-        clear_node(&rpool_rect[index]);
-	rpool_rect[index].next = &rpool_rect[index + 1];
+        clear_node(&rpool_nodes[index + 1]);
+        rpool_nodes[index].pool_index = index;
+	rpool_nodes[index].next = &rpool_nodes[index + 1];
     }
-    rpool_rect[index].next = NULL;
-    //rlist_head = &rpool_rect[0];
-    *rpool_rect = &rpool_rect[0];
-}
+    rpool_nodes[index].next = NULL;
+    //rlist_head = &rpool_nodes[0];
+    *rpool_pptr = &rpool_nodes[0];    /* To assign the HEADER Pointer */
+} 
 
-/* Function to Display the Array like Linked-list
-void display_rpool(Rectangle *rpool_ptr,int p_size)
+
+/* Function to Display the Array like Linked-list */
+void display_rpool(Rectangle *rlist_head)
 {
-    
-    int index, count;
-    while(rpool -> next != NULL)
+    int index, count = 0;
+    while(rlist_head -> next != NULL)
     {
         count += 1;
-	rpool += 1;
-	printf("\n X and Y co-ordinates of Rectangle are : \n (X,Y) = (%d,%d)", rpool_ptr.vertex[index].x, rpool_ptr.vertex[index].y);
+	rlist_head++;
+	//printf("\n X and Y co-ordinates of Rectangle are : \n (X,Y) = (%d,%d)", rlist_head->vertex[index].x, rlist_head->vertex[index].y);
     }
     printf(" Number of elements are : %d ", count);
 }
--- 2023_06_21/array_linkedlist.h
+++ 2023_06_21/array_linkedlist.h
@@ -1,5 +1,5 @@
-#ifndef _ARRAYLIST_H_
-#define _ARRAYLIST_H_
+#ifndef _ARRAY_LINKEDLIST_H_
+#define _ARRAY_LINKEDLIST_H_
 
 #define POOLSIZE 512
 #define VTX_COUNT 4
@@ -16,6 +16,7 @@ typedef struct Rectangle_t
     Point vertex[VTX_COUNT];
     char desc[DESC_LEN];
     int id;
+    int pool_index;
     struct Rectangle_t *next;
     struct Rectangle_t *prev;
 }Rectangle;
